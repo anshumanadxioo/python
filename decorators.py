26816ec3a4c17136aa2454dc065b29fab16a5540
anshumanadixoo <anshuman.mishra@adixoo.com>
2024-08-07 20:19:30 +0530

    added next example in the decorator file

--- decorators.py
+++ decorators.py
@@ -1,15 +1,31 @@
 import time
-def timer(func):
-    def wrapper(*args,**kwargs):
-        start=time.time();
-        res=func(*args,**kwargs);
-        end=time.time();
-        print(f"{func.__name__} ran in {end-start} time")
-        return res
-    return wrapper;
-@timer
-def example_function(n):
-    print("start..");
-    time.sleep(n)
-    print("end..")
-example_function(5);
+# def timer(func):
+#     def wrapper(*args,**kwargs):
+#         start=time.time();
+#         res=func(*args,**kwargs);
+#         end=time.time();
+#         print(f"{func.__name__} ran in {end-start} time")
+#         return res
+#     return wrapper;
+# @timer
+# def example_function(n):
+#     print("start..");
+#     time.sleep(n)
+#     print("end..")
+# example_function(5);
+
+
+# 2....................................................
+def debugger(func):
+    def wrapper(param,**params):
+        res=func(param,**params);
+        for ky,ind in params.items():
+            print(f"{ky} and the value is:-{ind}");
+        return res;
+    return wrapper
+@debugger
+def greet(name,greeting="hello"):
+    print(f"!{greeting} from {name}")
+greet("anshuman",greeting="hanji")
+
+
